4b63520ea73396f133ed796b3c02630fcae9dc07
Satyam Ghodasara <sghodas@gmail.com>
2015-12-30 23:11:23 -0500

    Addded checks for the removed card for draw test

--- src/deck/tests.rs
+++ src/deck/tests.rs
@@ -7,6 +7,14 @@ fn num_cards() {
 #[test]
 fn draw_card() {
     let mut deck = super::Deck::new();
-    let _card = deck.draw();
+    let top_card_value = deck.cards.get(51).unwrap().value.clone();
+    let top_card_suit = deck.cards.get(51).unwrap().suit.clone();
+    let card = deck.draw();
     assert_eq!(deck.cards.len(), 51);
+
+    assert!(card.value != ::card::Value::None);
+    assert!(card.suit != ::card::Suit::None);
+
+    assert_eq!(card.value, top_card_value);
+    assert_eq!(card.suit, top_card_suit);
 }
