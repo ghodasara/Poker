5026e3b7bfd70b2040c4e084a819c09fcd8d76d3
Satyam Ghodasara <sghodas@gmail.com>
2015-12-30 23:23:10 -0500

    Added unit test for drawing too many cards

--- src/deck/tests.rs
+++ src/deck/tests.rs
@@ -5,11 +5,11 @@ fn num_cards() {
 }
 
 #[test]
-fn draw_card() {
+fn draw_one_card() {
     let mut deck = super::Deck::new();
     let top_card_value = deck.cards.get(51).unwrap().value.clone();
     let top_card_suit = deck.cards.get(51).unwrap().suit.clone();
-    let card = deck.draw();
+    let card = deck.draw().unwrap();
     assert_eq!(deck.cards.len(), 51);
 
     assert!(card.value != ::card::Value::None);
@@ -17,4 +17,20 @@ fn draw_card() {
 
     assert_eq!(card.value, top_card_value);
     assert_eq!(card.suit, top_card_suit);
+}
+
+#[test]
+fn draw_too_many_cards() {
+    let mut deck = super::Deck::new();
+    for i in 0..53 {
+        let exists = match deck.draw() {
+            None    => false,
+            Some(_) => true
+        };
+        if i < 52 {
+            assert!(exists);
+        } else {
+            assert!(!exists);
+        }
+    }
 }
