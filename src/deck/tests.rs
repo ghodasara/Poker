9fce6edc2cda68777ab0e182f3ca1b04b87d10ab
Satyam Ghodasara <sghodas@gmail.com>
2015-12-30 22:59:14 -0500

    Added draw_card test

--- src/deck/tests.rs
+++ src/deck/tests.rs
@@ -3,3 +3,10 @@ fn num_cards() {
     let deck = super::Deck::new();
     assert_eq!(deck.cards.len(), 52);
 }
+
+#[test]
+fn draw_card() {
+    let mut deck = super::Deck::new();
+    let _card = deck.draw();
+    assert_eq!(deck.cards.len(), 51);
+}
