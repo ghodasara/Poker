c18ea9f8e3ff62fe1e685bbb7fd24f3e5b0b2e7f
Satyam Ghodasara <sghodas@gmail.com>
2015-12-30 22:19:04 -0500

    Re-implemented Display trait for Value enum

--- src/card.rs
+++ src/card.rs
@@ -24,7 +24,23 @@ pub enum Value {
 
 impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self)
+        let value = match *self {
+            Value::Two      =>  "2",
+            Value::Three    =>  "3",
+            Value::Four     =>  "4",
+            Value::Five     =>  "5",
+            Value::Six      =>  "6",
+            Value::Seven    =>  "7",
+            Value::Eight    =>  "8",
+            Value::Nine     =>  "9",
+            Value::Ten      =>  "T",
+            Value::Jack     =>  "J",
+            Value::Queen    =>  "Q",
+            Value::King     =>  "K",
+            Value::Ace      =>  "A",
+            Value::None     =>  "-"
+        };
+        write!(f, "{}", value)
     }
 }
 
