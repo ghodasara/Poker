7ed985355f5a0986e3f0ed31abe9e8d93e96b474
Satyam Ghodasara <sghodas@gmail.com>
2015-12-31 13:43:51 -0500

    Added test attribute to test module imports and added has_flush test

--- src/hand/tests.rs
+++ src/hand/tests.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+use ::card::*;
+
 #[test]
 fn create_hand() {
     let mut deck = ::deck::Deck::new();
@@ -8,4 +11,32 @@ fn create_hand() {
     let hand = ::hand::Hand::new(cards);
 
     assert_eq!(hand.cards.len(), 5);
+}
+
+#[test]
+fn has_flush() {
+    let a = Card {
+        value: Value::Three,
+        suit: Suit::Hearts
+    };
+    let b = Card {
+        value: Value::Six,
+        suit: Suit::Hearts
+    };
+    let c = Card {
+        value: Value::Ten,
+        suit: Suit::Hearts
+    };
+    let d = Card {
+        value: Value::Queen,
+        suit: Suit::Hearts
+    };
+    let e = Card {
+        value: Value::Two,
+        suit: Suit::Hearts
+    };
+    let cards: Vec<Card> = vec![a, b, c, d, e];
+    let hand: ::hand::Hand = ::hand::Hand::new(cards);
+
+    assert_eq!(hand.has_flush(), true);
 }
