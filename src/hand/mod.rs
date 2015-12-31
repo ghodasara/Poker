22270d3f0dfd3d1c32830e54ca10eb309bacd4ab
Satyam Ghodasara <sghodas@gmail.com>
2015-12-31 01:57:44 -0500

    Added Hand struct

---
+++ src/hand/mod.rs
@@ -0,0 +1,13 @@
+use card;
+
+pub struct Hand {
+    pub cards: Vec<card::Card>
+}
+
+impl Hand {
+    pub fn new(cards: Vec<card::Card>) -> Hand {
+        Hand {
+            cards: cards
+        }
+    }
+}
