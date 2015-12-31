f8918b580d9a8b6f89639efea182ce1898c64c2a
Satyam Ghodasara <sghodas@gmail.com>
2015-12-31 13:44:45 -0500

    Added test for not flush

--- src/hand/tests.rs
+++ src/hand/tests.rs
@@ -40,3 +40,31 @@ fn has_flush() {
 
     assert_eq!(hand.has_flush(), true);
 }
+
+#[test]
+fn no_flush() {
+    let a = Card {
+        value: Value::Three,
+        suit: Suit::Hearts
+    };
+    let b = Card {
+        value: Value::Six,
+        suit: Suit::Spades
+    };
+    let c = Card {
+        value: Value::Ten,
+        suit: Suit::Hearts
+    };
+    let d = Card {
+        value: Value::Queen,
+        suit: Suit::Hearts
+    };
+    let e = Card {
+        value: Value::Two,
+        suit: Suit::Hearts
+    };
+    let cards: Vec<Card> = vec![a, b, c, d, e];
+    let hand: ::hand::Hand = ::hand::Hand::new(cards);
+
+    assert_eq!(hand.has_flush(), false);
+}
