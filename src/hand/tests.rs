a0ddd3007caca065b15af6b1c57e656455c447f7
Satyam Ghodasara <sghodas@gmail.com>
2015-12-31 21:45:09 -0500

    Updated hand unit tests to use the Card constructor

--- src/hand/tests.rs
+++ src/hand/tests.rs
@@ -15,26 +15,11 @@ fn create_hand() {
 
 #[test]
 fn has_flush() {
-    let a = Card {
-        value: Value::Three,
-        suit: Suit::Hearts
-    };
-    let b = Card {
-        value: Value::Six,
-        suit: Suit::Hearts
-    };
-    let c = Card {
-        value: Value::Ten,
-        suit: Suit::Hearts
-    };
-    let d = Card {
-        value: Value::Queen,
-        suit: Suit::Hearts
-    };
-    let e = Card {
-        value: Value::Two,
-        suit: Suit::Hearts
-    };
+    let a = Card::new(Value::Three, Suit::Hearts);
+    let b = Card::new(Value::Six, Suit::Hearts);
+    let c = Card::new(Value::Ten, Suit::Hearts);
+    let d = Card::new(Value::Queen, Suit::Hearts);
+    let e = Card::new(Value::Two, Suit::Hearts);
     let cards: Vec<Card> = vec![a, b, c, d, e];
     let hand: ::hand::Hand = ::hand::Hand::new(cards);
 
@@ -43,28 +28,18 @@ fn has_flush() {
 
 #[test]
 fn no_flush() {
-    let a = Card {
-        value: Value::Three,
-        suit: Suit::Hearts
-    };
-    let b = Card {
-        value: Value::Six,
-        suit: Suit::Spades
-    };
-    let c = Card {
-        value: Value::Ten,
-        suit: Suit::Hearts
-    };
-    let d = Card {
-        value: Value::Queen,
-        suit: Suit::Hearts
-    };
-    let e = Card {
-        value: Value::Two,
-        suit: Suit::Hearts
-    };
+    let a = Card::new(Value::Three, Suit::Hearts);
+    let b = Card::new(Value::Six, Suit::Spades);
+    let c = Card::new(Value::Ten, Suit::Hearts);
+    let d = Card::new(Value::Queen, Suit::Hearts);
+    let e = Card::new(Value::Two, Suit::Hearts);
     let cards: Vec<Card> = vec![a, b, c, d, e];
     let hand: ::hand::Hand = ::hand::Hand::new(cards);
 
     assert_eq!(hand.has_flush(), false);
+}
+
+#[test]
+fn high_card_raw_value() {
+
 }
