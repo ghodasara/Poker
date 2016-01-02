c9043cd67031b5c259a9bedfeff7157a08ec9ef4
Satyam Ghodasara <sghodas@gmail.com>
2016-01-02 00:58:11 -0500

    Removed explicit enum values for Card Ranks

--- src/card/mod.rs
+++ src/card/mod.rs
@@ -39,19 +39,19 @@ impl Card {
 
 #[derive(Debug, Copy, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub enum Rank {
-    Two     =   2,
-    Three   =   3,
-    Four    =   4,
-    Five    =   5,
-    Six     =   6,
-    Seven   =   7,
-    Eight   =   8,
-    Nine    =   9,
-    Ten     =   10,
-    Jack    =   11,
-    Queen   =   12,
-    King    =   13,
-    Ace     =   14
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
 }
 
 impl fmt::Display for Rank {
